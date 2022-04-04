68c3891c5771b0e271e60d0a3b2a8c953c19a24f
Elizaveta <elizavetaivanushenko@gmail.com>
2022-04-04 10:25:57 +0300

    refactor:

--- src/app/filtering-block/filtering-block.component.ts
+++ src/app/filtering-block/filtering-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filtering-block',
@@ -6,19 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./filtering-block.component.scss'],
 })
 export class FilteringBlockComponent  {
-  //@Input() isVisible?: boolean;
 
   constructor() { 
-    
   }
 
   ngOnInit(): void {
-
   }
-  sort() {
-    console.log('sorted');
-    this.ngOnInit();
-  }
-  
 
 }
